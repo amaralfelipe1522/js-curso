10791cc49e4de084a5c1da2d4edafae18b055306
amaralfelipe1522 <amaral.felipe@live.com>
2020-11-23 13:43:46 -0300

    Adding class 0201

---
+++ 02-javascript-para-iniciantes/0201-variaveis/script.js
@@ -0,0 +1,16 @@
+console.log("Hello World!!");
+
+let nome = "Felipe";
+var idade = 29;
+const possuiFaculdade = true;
+
+console.log(nome+"\n"+idade+"\n"+possuiFaculdade);
+
+let sobreNome = " Amaral",
+    nomeCompleto = nome+sobreNome;
+
+console.log(nomeCompleto);
+//Sem definição
+console.log(semDefinir);
+var semDefinir = true;
+console.log(semDefinir);
