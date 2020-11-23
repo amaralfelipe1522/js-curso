d2a3fdf78d1e8689ea64844faaa6f628cb936308
amaralfelipe1522 <amaral.felipe@live.com>
2020-11-23 14:21:36 -0300

    Adding class 0202

---
+++ 02-javascript-para-iniciantes/0202-tipos-de-dados/script.js
@@ -0,0 +1,21 @@
+// Declare uma variável contendo uma string
+let palavra = "Xablau";
+console.log(palavra);
+// Declare uma variável contendo um número dentro de uma string
+let dataAtual = "23 de novembro de 2020"
+console.log(dataAtual);
+// Declare uma variável com a sua idade
+let idade = 29;
+console.log(idade);
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+let nome = "Felipe",
+    sobrenome = "Amaral",
+    nomeCompleto = `${nome} ${sobrenome}`
+console.log(nomeCompleto);
+// Coloque a seguinte frase em uma variável: It's time
+let frase = "It's time"
+console.log(frase);
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome);
+
