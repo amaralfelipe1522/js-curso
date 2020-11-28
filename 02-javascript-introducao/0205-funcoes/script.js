501b185209a4fc3e2eef99d580ec72e6dbc846ac
amaralfelipe1522 <amaral.felipe@live.com>
2020-11-28 12:51:55 -0300

    Adding class 0205-2

--- 02-javascript-introducao/0205-funcoes/script.js
+++ 02-javascript-introducao/0205-funcoes/script.js
@@ -37,3 +37,90 @@ calcularImc();
 addEventListener("click", function(){
     console.log("Clicou");
 });
+
+function verificaFaixaEtaria(idade) {
+    let validador = isNaN(+idade);
+    if (validador == true || idade < 0) {
+        console.log("Informe um valor de idade válido.");
+    } else if (idade <= 3) {
+        console.log("Bebê");
+    } else if (idade < 13) {
+        console.log("Criança");
+    } else if (idade < 18){
+        console.log("Adolescente");
+    } else if (idade < 60){
+        console.log("Adulto");
+    } else {
+        console.log("Idoso");
+    }
+}
+
+verificaFaixaEtaria("64");
+
+// Crie uma função para verificar se um valor é Truthy
+
+function verificarBoo(valor) {
+    return !!valor
+}
+console.log(verificarBoo(NaN));
+
+// Crie uma função matemática que retorne o perímetro de um quadrado
+// lembrando: perímetro é a soma dos quatro lados do quadrado
+
+function calcularQuadrado(lado) {
+    let quadrado =  lado ** 2;
+    console.log(`O quadrado de ${lado} é ${quadrado}.`);
+}
+
+calcularQuadrado(16);
+
+// Crie uma função que retorne o seu nome completo
+// ela deve possuir os parâmetros: nome e sobrenome
+
+function nomeCompleto(nome, sobrenome) {
+    return `${nome} ${sobrenome}`
+}
+
+console.log(nomeCompleto("Felipe", "Amaral"));
+
+// Crie uma função que verifica se um número é par
+function isPar(valor){
+    let resto = valor % 2;
+    if (resto == 0) {
+        return "É Par"
+    } else {
+        return "É impar"
+    }
+}
+
+console.log(isPar(8));
+
+// Crie uma função que retorne o tipo de
+// dado do argumento passado nela (typeof)
+
+function identType(valor) {
+    return typeof valor
+}
+
+console.log(identType(true));
+
+// addEventListener é uma função nativa do JavaScript
+// o primeiro parâmetro é o evento que ocorre e o segundo o Callback
+// utilize essa função para mostrar no console o seu nome completo
+// quando o evento 'scroll' ocorrer.
+
+addEventListener("wheel", function() {
+    console.log(nomeCompleto("Felipe","Amaral"));
+});
+
+// Corrija o erro abaixo
+var totalPaises = 193;
+    function precisoVisitar(paisesVisitados) {
+        return `Ainda faltam ${totalPaises - paisesVisitados} países para visitar`;
+    }
+    function jaVisitei(paisesVisitados) {
+        return `Já visitei ${paisesVisitados} do total de ${totalPaises} países`;
+    }
+console.log(precisoVisitar(20));
+console.log(jaVisitei(20));
+  
