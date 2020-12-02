791e7459bc4ca63746a4ed9b54be4bf181528c1f
amaralfelipe1522 <amaral.felipe@live.com>
2020-12-02 09:08:11 -0300

    Adding class 0207

--- 02-javascript-introducao/0207-tudo-e-objeto/script.js
+++ 02-javascript-introducao/0207-tudo-e-objeto/script.js
@@ -30,11 +30,11 @@ console.log(nome.search("p"));
 console.log(nome.split("l"));
 
 // nomeie 5 propriedades ou métodos de elementos do DOM
-console.log();
-console.log();
-console.log();
-console.log();
-console.log();
+document.querySelector();
+document.parentNode;
+document.onselect
+document.onscroll
+document.onreadystatechange 
 
 // busque na web um objeto (método) capaz de interagir com o clipboard, 
 // clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
